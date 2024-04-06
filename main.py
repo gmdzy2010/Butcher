9731257381f3b210222c4657267db99f49b0a5c9
gmdzy2010 <gmdzy2010@126.com>
2024-04-07 02:23:08 +0800

    fix: 修复加载模型错误

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from os.path import exists, join
 
 from dotenv import dotenv_values
+from keras.models import load_model
 
-# from keras.models import load_model
 from image_classification.datasets import get_test_dataset
 from image_classification.models import (
-    get_trained_model,
     save_trained_model_to_file,
 )
 
@@ -16,13 +15,16 @@ if not model_path or not model_name:
 
 model_file = join(model_path, f"{model_name}.keras")
 if not exists(model_file):
-    save_trained_model_to_file(model_path, model_name)
+    model = save_trained_model_to_file(model_path, model_name)
+else:
+    model = load_model(model_file)
+
 
 # ! 这里保存训练过的模型再次加载会报错，原因未知
-# model = load_model(model_file)
-model = get_trained_model()
-print(model.summary())
+# * 是 Python 3.12.0 的原因
+# * https://github.com/tensorflow/tensorflow/issues/63365
+print(model.summary())  # type: ignore
 
 test_images, test_labels = get_test_dataset()
-test_loss, test_accu = model.evaluate(test_images, test_labels)
+test_loss, test_accu = model.evaluate(test_images, test_labels)  # type: ignore
 print(test_loss, test_accu)
